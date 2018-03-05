44131841ca18b6c88776d7ed3a7ee527d2d4d191
Roger Williams <roger.kwilliams93@gmail.com>
2018-03-04 21:35:39 -0500

    Modified student navigator to return null, for now.

--- Components/Student/StudentNavigator.js
+++ Components/Student/StudentNavigator.js
@@ -1,12 +1,2 @@
-import React from 'react';
-import { StackNavigator } from 'react-navigation';
-import Home from './Home';
-
-export const StudentNavigator = StackNavigator({
-    Home: {
-      screen: Home
-    }
-  },
-  {
-    headerMode: 'none'
-});
+const StudentNavigator = null;
+export default StudentNavigator;
