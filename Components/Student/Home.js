a2f0517edb0d024a63626fbaa3358ea37e5ee5cd
sungjkim34 <sungjkim34@gmail.com>
2018-04-02 21:58:52 -0400

    students

--- Components/Student/Home.js
+++ Components/Student/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Dimensions, StyleSheet, Text, View, Image, ImageBackground, TouchableWithoutFeedback, Animated, Easing } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, Image, ImageBackground, TouchableWithoutFeedback, Animated, Easing, YellowBox } from 'react-native';
 import { Button, Input } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import DeviceInfo from 'react-native-device-info';
@@ -14,6 +14,9 @@ export default class Home extends Component {
 
     constructor() {
         super();
+        YellowBox.ignoreWarnings(['Setting a timer']);
+        YellowBox.ignoreWarnings(['Deprecation warning']);
+        YellowBox.ignoreWarnings(['Possible Unhandled Promise Rejection']);
         this.state = {
             deviceInfo: DeviceInfo.getUniqueID(),
             isLoading: true,
@@ -21,78 +24,25 @@ export default class Home extends Component {
             height: undefined,
             width: undefined,
             checkedIn: false,
+            checkinDisabled: true,
             circles: [],
             toggleViewDeviceInfo: false,
 
             shouldCheckTime: false,
             soonClasses: [],
+            classId: undefined
         };
-    }
-
-    _IsActiveClass = (startDate, endDate, startTime, endTime, classDets) => {
-        let currDT = moment();
-        let currentTime =  moment(currDT, 'hh:mm A');
-        const timeDiff = startTime.diff(currentTime, 'minutes');
-
-        const isActive = moment(moment()).isBetween(startDate, endDate);
-    
-        
-        const meetingDays = this._getClassMeetingTimes(classDets.meetingDays);
-
-        const shouldCheck = isActive && (timeDiff > 0 && timeDiff <= 120) && (meetingDays.includes(currDT.format('dddd')));
-
-        this.setState({
-            shouldCheckTime: shouldCheck
-        });
-
-        return shouldCheck;
-
-    }
-
-    _getClassMeetingTimes = (daysArray) => {
-        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        let classDays = [];
-
-        for(let i = 0; i < days.length; i++) {
-            if (daysArray[i]) {
-                classDays.push(days[i]);
-            } 
-        }
-        return classDays;
-    }
-
-    _findClassesToday = () => {
-
-        if (this.state.deviceData) {
-            for(_class in this.state.deviceData.classesEnrolled) {
-                ClassService.getClass(_class, (res) => {
-                    console.log("RES: ", res)
-                    let startDate = moment(res.startDate);
-                    let endDate = moment(res.endDate);
-
-                    let startTime = moment(res.startTime,  ['hh:mm A', 'HH:mm A']);
-                    let endTime = moment(res.endTime,  ['hh:mm A', 'HH:mm A']);
-
-                    let currentDateTime = moment();
-
-                    if (this._IsActiveClass(startDate, endDate, startTime, endTime, res)) {
-                        console.log("this class");
-                        let sclasses = this.state.soonClasses.splice();
-                        sclasses.push(res);
-
-                        this.setState({
-                            soonClasses: sclasses
-                        });
-
-                    }
-
-                });
-            }
-        }
     }
 
     componentWillMount() {
         const { width, height } = Dimensions.get('window');
+
+        ClassService.getClassWithTime(this.state.deviceInfo).then(classId => {
+            this.setState({ checkinDisabled: !classId, classId });
+            ClassService.checkIfAlreadyCheckedIn(classId, this.state.deviceInfo).then(res => {
+                this.setState({checkedIn: res});
+            });
+        });
 
         DeviceService.getDevice(this.state.deviceInfo, (res) => {
             if (res == null) {
@@ -112,31 +62,34 @@ export default class Home extends Component {
                 width: width,
                 height: height
             });
-
-            this._findClassesToday();
         });
 
-        console.log(this.state);
+        // console.log(this.state);
         if(this.state.shouldCheckTime) {
             console.log("OKay immm");
         }
         setInterval(() => {
-            // console.log(this.state.soonClasses);
-            // for (_class in this.state.soonClasses) {
-            //     let currentTime =  moment(moment(), 'hh:mm A');
-            //     let startTime = moment(this.state.soonClasses[_class].startTime,  ['hh:mm A', 'HH:mm A']);
-            //     let endTime = moment(this.state.soonClasses[_class].endTime,  ['hh:mm A', 'HH:mm A']);
-
-            //     console.log("Time bet: ", currentTime.isBetween(startTime, endTime));
-
-            // }
-
             this.setState({ circles: [...this.state.circles, 1] });
         }, 2000);
     }
 
+    refresh() {
+        ClassService.getClassWithTime(this.state.deviceInfo)
+            .then(classId => {
+                this.setState({ checkinDisabled: !classId, classId });
+            })
+            .catch(err => {
+                this.setState({ checkinDisabled: true });
+            });
+    }
+
     checkIn() {
-        this.setState({checkedIn: !this.state.checkedIn})
+        if(this.state.classId && !this.state.checkedIn) {
+            ClassService.classCheckIn(this.state.classId, this.state.deviceInfo);
+            if (!this.state.checkinDisabled) {
+                this.setState({checkedIn: !this.state.checkedIn});
+            }
+        }
     }
 
     render() {
@@ -152,17 +105,21 @@ export default class Home extends Component {
                         {
                             this.state.checkedIn ?
                                 <Icon name='check' size={75} color='#7DD892' /> :
-                                <Image source={require('../../Assets/trackify-logo2.png')} style={{height: 100, width: 100}}/>
+                                    this.state.checkinDisabled ?
+                                        <Icon name='close' size={80} color='rgb(234, 98, 121)' /> :
+                                        <Image source={require('../../Assets/trackify-logo2.png')} style={{height: 100, width: 100}}/>
                         }
                     </View>
                     </TouchableWithoutFeedback>
-                    {!this.state.checkedIn && this.state.circles.map((circle, i) => <Pulse key={i}/>)}
+                    {!this.state.checkedIn && !this.state.checkinDisabled && this.state.circles.map((circle, i) => <Pulse key={i}/>)}
                 </View>
                 <View style={{flex: 1}}>
                     {
                         this.state.checkedIn ?
                             <Text style={{fontSize: 18}}>You have successfully checked in to class</Text> :
-                            <Text style={{fontSize: 18}}>You have not yet checked in to class</Text>
+                            this.state.checkinDisabled ? 
+                                <Text style={{fontSize: 18}}>You do not have any classes during current time</Text> :
+                                <Text style={{fontSize: 18}}>You have not yet checked in to class</Text>
                     }
                 </View>
                 <View style={{flex: 1, flexDirection: 'row', paddingHorizontal: 25, alignItems: 'center'}}>
@@ -170,7 +127,7 @@ export default class Home extends Component {
                         <Icon name='tablet' size={35} onPress={() => this.setState({toggleViewDeviceInfo: true})} />
                     </View>
                     <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-end'}}>
-                        <Icon name='info-circle' size={35} />
+                        <Icon name='refresh' size={35} onPress={() => this.refresh()} />
                     </View>
                 </View>
                 <Modal
