527ebea79fc28ae0b382dbfec9d6ab60477edc47
sungjkim34 <sungjkim34@gmail.com>
2018-04-02 21:58:52 -0400

    students

--- Components/Professor/Home.js
+++ Components/Professor/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, StyleSheet, ActivityIndicator } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, ActivityIndicator, YellowBox } from 'react-native';
 import { Button, Icon, List, ListItem } from 'react-native-elements';
 import AuthService from '../../Services/AuthService';
 import UserService from '../../Services/UserService';
@@ -27,6 +27,8 @@ export default class Home extends Component {
     
     constructor(props) {
         super(props);
+
+        YellowBox.ignoreWarnings(['Setting a timer']);
 
         this.state = {
             isLoading: true,
